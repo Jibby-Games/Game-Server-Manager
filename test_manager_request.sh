5681c0ba5931f7a63f337a601306fffc78b02812
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2026-04-05 16:33:26 +0100

    feat: bump version to 0.4.0

--- test_manager_request.sh
+++ test_manager_request.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # POST request to start a server
 curl -H 'Content-Type: application/json' \
-    -d '{ "name":"test-server","list":1, "version": "0.13.0"}' \
+    -d '{ "name":"test-server","list":1, "version": "0.14.0"}' \
     -X POST -k \
     localhost:8000/api/manager/request
